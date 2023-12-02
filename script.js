e57ee587383eb23999e93b05e044a30cf10393ea
ubaidullah-786 <144231780+ubaidullah-786@users.noreply.github.com>
2023-12-02 17:32:26 +0500

    Grabed all DOM elements

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
